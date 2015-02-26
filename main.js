82b522677184c34ca2931c289d3be981ee2bb976
tamatsu <gtamatsu@gmail.com>
2015-02-26 03:40:18 -0500

    modify index.html main.js

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
 window.onload = function() {
-	window.alert("hello");
+//	window.alert("hello");
+	var canvas = document.getElementById('canvas');
+	
+	//canvas.width = 40;
+	canvas.width = 320;
+	canvas.height = 240;
+	
+	var ctx = canvas.getContext('2d');
+  	ctx.fillRect(10, 10, 50, 50);
+  	
+  	var gl = canvas.getContext("webgl");
+  	//var gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');  	
+  	
+  	if (gl == null) {
+  		window.alert("failed to get webgl context");
+  	}
+  	
+  	
+  	
+  	gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  	gl.clear(gl.COLOR_BUFFER_BIT);
+
+	
 };
